554ad83a178350af717e03ca03ae05fdb3621230
Jeremy Bertus <jeremy@gethardwirednow.com>
2025-05-06 01:35:12 -0500

    Fix: Full Vercel TypeScript support

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+
+/// <reference types="next/navigation-types/compat/navigation" />
+
+// NOTE: This file should not be edited
+// See https://nextjs.org/docs/basic-features/typescript for more information.
